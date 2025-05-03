ee8be63db085154685b2b818425a80260e664b0b
scalalang2 <cailhuiris@gmail.com>
2025-05-03 19:44:52 +0900

    changed the game to be run by BackgroundService

--- Application/SampleServer/Program.cs
+++ Application/SampleServer/Program.cs
@@ -1,43 +1,11 @@
-﻿using System.Net;
-using System.Net.Sockets;
-using System.Text;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using SampleServer;
 
-const int port = 9000;
-var server = new UdpClient(port);
+var builder = WebApplication.CreateBuilder(args);
 
-Console.WriteLine($"Server started on port {port}. Waiting for client...");
+builder.Services.AddHostedService<SampleGameService>();
 
-while (true)
-{
-    var remoteEp = new IPEndPoint(IPAddress.Any, 0);
-    var recv = server.Receive(ref remoteEp);
-    var clientChoice = Encoding.UTF8.GetString(recv).Trim();
-    
-    string[] choices = { "Scissors", "Paper", "Rock" };
-    var rnd = new Random();
-    var serverChoice = choices[rnd.Next(choices.Length)];
-
-    var result = GetResult(clientChoice, serverChoice);
-    var response = $"Server: {serverChoice}, Result: {result}\n";
-    var sendBytes = Encoding.UTF8.GetBytes(response);
-    server.Send(sendBytes, sendBytes.Length, remoteEp);
-}
-string GetResult(string client, string server)
-{
-    if (client == server)
-        return "Draw";
-    
-    switch (client)
-    {
-        case "Scissors" when server == "Rock":
-        case "Paper" when server == "Scissors":
-        case "Rock" when server == "Paper":
-            return "Client Win";
-        case "Scissors" when server == "Paper":
-        case "Paper" when server == "Rock":
-        case "Rock" when server == "Scissors":
-            return "Server Win";
-        default:
-            return "Invalid Choice from Client, Available Choices are [Rock, Paper, Scissors]";
-    }
-}
+var app = builder.Build();
+app.MapGet("/", () => "Hello :)");
+app.Run();
---
+++ Application/SampleServer/SampleGameService.cs
@@ -0,0 +1,49 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Microsoft.Extensions.Hosting;
+
+namespace SampleServer;
+
+public class SampleGameService : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        const int port = 9000;
+        using var server = new UdpClient(port);
+
+        Console.WriteLine($"Server started on port {port}. Waiting for client...");
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var recv = await server.ReceiveAsync(stoppingToken);
+            var clientChoice = Encoding.UTF8.GetString(recv.Buffer).Trim();
+
+            string[] choices = { "Scissors", "Paper", "Rock" };
+            var rnd = new Random();
+            var serverChoice = choices[rnd.Next(choices.Length)];
+
+            var result = GetResult(clientChoice, serverChoice);
+            var response = $"Server: {serverChoice}, Result: {result}\n";
+            var sendBytes = Encoding.UTF8.GetBytes(response);
+            await server.SendAsync(sendBytes, sendBytes.Length, recv.RemoteEndPoint);
+        }
+    }
+
+    private string GetResult(string client, string server)
+    {
+        if (client == server)
+            return "Draw";
+
+        return (client, server) switch
+        {
+            ("Scissors", "Rock") => "Server Win",
+            ("Paper", "Scissors") => "Server Win",
+            ("Rock", "Paper") => "Server Win",
+            ("Scissors", "Paper") => "Client Win",
+            ("Paper", "Rock") => "Client Win",
+            ("Rock", "Scissors") => "Client Win",
+            _ => "Invalid Choice from Client, Available Choices are [Rock, Paper, Scissors]"
+        };
+    }
+}
